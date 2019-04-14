bd074a6b27baf327b1c89d8061979a049e9012bb
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-04-14 15:33:46 -0400

    strlen

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - finds len of string
+ * @str: string to mesure
+ *
+ * Return: size of string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while(str)
+	{
+		len++;
+		str++;
+	}
+}
